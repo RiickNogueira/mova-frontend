643b8ff361a7e8032caf7aa6b061703f441539a3
Richardson <richardsonogueira@gmail.com>
2021-04-07 12:03:54 -0300

    ajustando resultado unico

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,9 @@
       </v-col>
       <v-col class="d-flex" cols="12" sm="4"
         ><v-autocomplete
-          :label="`${filterOption ? filterOption.name : ''}`"
+          :label="`${
+            filterOption ? filterOption.name : 'Selecione o tipo do filtro'
+          }`"
           v-model="filter"
           :items="options"
           :item-text="`${filterOption ? filterOption.field : ''}`"
@@ -26,43 +28,55 @@
         ></v-autocomplete
       ></v-col>
       <v-col class="d-flex" cols="12" sm="4"
-        ><v-btn depressed rounded color="primary" @click="getCountrys">
+        ><v-btn depressed rounded color="primary" @click="getCountries">
           PESQUISAR
         </v-btn></v-col
       ></v-row
     >
-
-    <v-data-iterator
-      :items="countrys"
-      hide-default-footer
-      fluid
-      class="elevation-1"
-      :page.sync="page"
-      :items-per-page="itemsPerPage"
-      @page-count="pageCount = $event"
-    >
-      <template v-slot:default="props">
-        <v-row>
-          <v-col
-            v-for="(item, index) in props.items"
-            :key="index"
-            cols="12"
-            md="4"
-          >
-            <v-card>
-              <v-img
-                gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.3)"
-                height="200"
-                :src="item.flag"
-              ></v-img>
-            </v-card>
-          </v-col>
-        </v-row>
-      </template>
-    </v-data-iterator>
-    <div class="text-center pt-2">
-      <v-pagination v-model="page" :length="pageCount"></v-pagination>
+    <div v-if="countries.length > 1">
+      <v-data-iterator
+        :items="countries"
+        hide-default-footer
+        fluid
+        class="elevation-1"
+        :page.sync="page"
+        :items-per-page="itemsPerPage"
+        @page-count="pageCount = $event"
+      >
+        <template v-slot:default="props">
+          <v-row>
+            <v-col
+              v-for="(item, index) in props.items"
+              :key="index"
+              cols="12"
+              md="4"
+            >
+              <v-card>
+                <v-img
+                  gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.3)"
+                  height="200"
+                  :src="item.flag"
+                ></v-img>
+              </v-card>
+            </v-col>
+          </v-row>
+        </template>
+      </v-data-iterator>
+      <div class="text-center pt-2">
+        <v-pagination v-model="page" :length="pageCount"></v-pagination>
+      </div>
     </div>
+    <div v-else-if="countries.length == 1" class="pa-2 d-flex justify-center">
+      <v-col cols="12" md="4">
+        <v-card>
+          <v-img
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.3)"
+            height="200"
+            :src="countries[0].flag"
+          ></v-img> </v-card
+      ></v-col>
+    </div>
+    <div v-else>Nenhum resultado</div>
   </v-container>
 </template>
 
@@ -77,7 +91,7 @@ export default {
       pageCount: 0,
       itemsPerPage: 12,
       options: [],
-      countrys: [],
+      countries: [],
       filter: null,
       filterOption: null,
       filterOptions: [
@@ -119,12 +133,14 @@ export default {
         console.log(e);
       }
     },
-    async getCountrys() {
+    async getCountries() {
+      const bool = true;
+      if (!this.filter && !this.filterOption) alert("Filtro inválido");
       try {
         const response = await axios.get(
           `${this.filterOption.api}${this.filter}`
         );
-        this.countrys = response.data;
+        this.countries = response.data;
       } catch (e) {
         console.log(e);
       }
